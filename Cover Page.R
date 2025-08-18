ea3553187e248c82464208f0b7e7611298913ec3
saifuddin-statlab <saif86.ctg@gmail.com>
2025-08-18 22:37:31 +0600

    Updated, Fall 2025

--- Cover Page.R
+++ Cover Page.R
@@ -12,3 +12,18 @@ ggpairs(mtcars, columns = c(1,3,4,6), ggplot2::aes(alpha=0.5,colour=factor(vs)),
   theme_stata()
 
 
+mtcars$vs <- factor(
+  mtcars$vs,
+  levels = c(0, 1),
+  labels = c("V-shaped", "Straight")
+)
+
+
+ggpairs(
+  mtcars,
+  columns = c(1, 3, 4, 6),
+  ggplot2::aes(alpha = 0.5, colour = vs),
+  columnLabels = c("MPG", "Displacement", "Horse power", "Weight")
+) +
+  theme_stata() +
+  scale_color_stata()
